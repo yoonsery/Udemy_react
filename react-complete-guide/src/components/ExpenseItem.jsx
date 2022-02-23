b19653a75af2789e85254039062ba64cf96a921c
sery <yunsel2@gmail.com>
2022-02-23 16:07:53 +0900

    feat: Separate ExpenseDate from ExpenseItem component

--- react-complete-guide/src/components/ExpenseItem.jsx
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -1,11 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2022, 1, 22).toISOString();
-
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
