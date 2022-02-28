b14b2b4407caae028ab8437a97b85263eeaf58fb
sery <yunsel2@gmail.com>
2022-02-28 18:19:18 +0900

    refactor: Get a data list dynamically

--- react-complete-guide/src/components/Expenses/Expenses.jsx
+++ react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -18,26 +18,14 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {props.items.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
     </Card>
   );
 }
