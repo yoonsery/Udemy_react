b3c6659ede6fd42cfb15768fe7e1a0ba39f929ba
sery <yunsel2@gmail.com>
2022-03-08 22:13:38 +0900

    feat: How to use useEffect hook_ Add log in, log out features

--- react_useEffect/src/App.js
+++ react_useEffect/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedLogInInfo = localStorage.getItem('isLoggedIn');
+    if (storedLogInInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []); // callback will run only once when the app starts
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1'); // 0: not logged in, 1: logged in
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
