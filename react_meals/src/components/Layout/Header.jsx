09c7eba8f708c5ef945f7d13e87a28f325504832
sery <yunsel2@gmail.com>
2022-03-12 23:37:19 +0900

    feat: Add cart icon and button

--- react_meals/src/components/Layout/Header.jsx
+++ react_meals/src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import mealsImage from '../../assets/sara-dubler_unsplash.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 function Header() {
   return (
     <>
       <header className={styles.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt="vegetables and breaded dishes" />
