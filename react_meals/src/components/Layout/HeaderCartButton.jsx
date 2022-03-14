bf80e3bb0aee3f7b70ed906ca75d8f084d4d8084
sery <yunsel2@gmail.com>
2022-03-15 00:15:20 +0900

    feat: Adapt useContext at headerCartButton

--- react_meals/src/components/Layout/HeaderCartButton.jsx
+++ react_meals/src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart_context';
 
 function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />{' '}
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
