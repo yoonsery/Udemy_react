fe735f9b2e68ff0380e1b80878f1a1d3274fe606
sery <yunsel2@gmail.com>
2022-06-16 22:14:34 +0900

    How to get entered value with state and ref

--- react_form_input/src/components/SimpleInput.js
+++ react_form_input/src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName(''); // Reset entered value
+    // nameInputRef.current.value = ''; 💩 Do not manipulate DOM
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          ref={nameInputRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
