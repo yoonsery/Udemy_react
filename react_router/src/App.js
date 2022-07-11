ce2bae8d0b67781cc8647dce10eb94f33cb45553
sery <yunsel2@gmail.com>
2022-07-11 17:45:24 +0900

    How to use router link

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,7 +1,21 @@
+import { Route } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
