8e30fa904a71f3036ce80274ffd8de206f051474
sery <yunsel2@gmail.com>
2022-07-12 23:51:40 +0900

    feat: How to output data on the detail page

--- react_router_project/src/pages/QuoteDetail.jsx
+++ react_router_project/src/pages/QuoteDetail.jsx
@@ -1,16 +1,26 @@
 import { Route, useParams } from 'react-router-dom';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
 
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Sery', text: 'Learning React is fun 🤡' },
+  { id: 'q2', author: 'Harry', text: 'Learning React is cool 😎' },
+];
+
 const QuoteDetail = () => {
-  const param = useParams();
-  console.log(param.quoteId);
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found 🤔</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{param.quoteId}</p>
-      <Route path={`/quotes/${param.quoteId}/comments`}>
-        {/* <Route path="/quotes/:quoteId/comments"> */} you can also use this
-        code
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </>
