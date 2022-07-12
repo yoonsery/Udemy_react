e5e6892ac5fcb26dd8efa05bcc779ea452123db4
sery <yunsel2@gmail.com>
2022-07-12 13:48:01 +0900

    feat: How to use redirect

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -11,6 +11,9 @@ function App() {
 
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
           <Route path="/welcome">
             <Welcome />
           </Route>
