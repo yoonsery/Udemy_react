8075e7283d0ca3dec3a14ff6c9ceeab76fb91207
sery <yunsel2@gmail.com>
2022-07-19 14:41:12 +0900

    feat: Remove dummy data and get & send quote data from firebase visa Http

--- react_router_project/src/pages/QuoteDetail.jsx
+++ react_router_project/src/pages/QuoteDetail.jsx
@@ -1,26 +1,49 @@
 import { Link, Route, useParams, useRouteMatch } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import { getSingleQuote } from '../lib/api';
 import Comments from '../components/comments/Comments';
-
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Sery', text: 'Learning React is fun 🤡' },
-  { id: 'q2', author: 'Harry', text: 'Learning React is cool 😎' },
-];
+import useHttp from '../hooks/use-http';
+import { useEffect } from 'react';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
 const QuoteDetail = () => {
   const params = useParams();
   const match = useRouteMatch();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params; // 🔥 destructuring을 해서 useEffect 의 dependancy에 params 전체를 추가할 필요가 없다!! params가 여러개일때 예상치 못하게 렌더되는 부작용을 막을 수 있음
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === 'error') {
+    return <p className="centered ">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found 🤔</p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
