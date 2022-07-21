469274154c2dc1bdca69229f7f8d38fa76f5773f
sery <yunsel2@gmail.com>
2022-07-21 18:04:37 +0900

    refactor: Add asterisk at the end to render nested comment route

--- react_router_project/src/App.js
+++ react_router_project/src/App.js
@@ -12,9 +12,9 @@ function App() {
       <Routes>
         <Route path="/*" element={<Navigate replace to="/quotes" />} />
         <Route path="/quotes/*" element={<AllQuotes />} />
-        <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
+        <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}>
           <Route
-            path={`*`}
+            path={'*'}
             element={
               <div className="centered">
                 <Link className="btn--flat" to="comments">
