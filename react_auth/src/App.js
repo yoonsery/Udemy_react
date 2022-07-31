b485d2a1787fba6e185e4d5b53838ba5f70b80ac
sery <yunsel2@gmail.com>
2022-08-01 00:30:09 +0900

    feat: Add user authentication status to local storage

--- react_auth/src/App.js
+++ react_auth/src/App.js
@@ -15,14 +15,12 @@ function App() {
       <Routes>
         <Route path="/*" element={<HomePage />} />
         {!authCtx.isLoggedIn && <Route path="/auth" element={<AuthPage />} />}
-        (
         <Route
           path="/profile"
           element={
             authCtx.isLoggedIn ? <UserProfile /> : <Navigate to="/auth" />
           }
         />
-        )
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Layout>
