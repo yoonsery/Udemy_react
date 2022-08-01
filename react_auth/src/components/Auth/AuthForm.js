2cec11073fc38fa16c02035b02c5ec13509ff907
sery <yunsel2@gmail.com>
2022-08-01 17:16:04 +0900

    feat: Add Auto logout feature

--- react_auth/src/components/Auth/AuthForm.js
+++ react_auth/src/components/Auth/AuthForm.js
@@ -59,7 +59,10 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken);
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
         navigate('/', { replace: true });
       })
       .catch((err) => {
