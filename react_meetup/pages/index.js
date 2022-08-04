409767295b3464d78e9479a7d867290b62bf60b1
sery <yunsel2@gmail.com>
2022-08-05 00:17:52 +0900

    feat: Add some meetup dummy list

--- react_meetup/pages/index.js
+++ react_meetup/pages/index.js
@@ -1,1 +1,27 @@
+import MeetupList from '../components/meetups/MeetupList';
 // my-domain.com/
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://images.unsplash.com/photo-1553604503-1ea63c3ba69c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1275&q=80',
+    address: 'Prague, Czechia',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://images.unsplash.com/photo-1565611487608-281f9c0cafb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
+    address: 'Rotterdam, Netherlands',
+    description: 'This is a second meetup!',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
