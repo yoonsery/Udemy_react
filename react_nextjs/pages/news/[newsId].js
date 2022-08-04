5240e2d7fc20d7fb4805770a3652ad9097b6f3e2
sery <yunsel2@gmail.com>
2022-08-04 17:36:34 +0900

    feat: How to use useRouter to extract dynamic parameter values

--- react_nextjs/pages/news/[newsId].js
+++ react_nextjs/pages/news/[newsId].js
@@ -1,6 +1,14 @@
+import { useRouter } from 'next/router';
+
 // my-domain.com/news/sth-important
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // Send a request to the backend API to fetch the news item with newsId
+
   return <h1>The Detail Page</h1>;
 }
 
