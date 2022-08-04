3f2eeb6a57c6c8932ac019302bb1315ec87225f3
sery <yunsel2@gmail.com>
2022-08-04 23:41:34 +0900

    feat: How to link between pages

--- react_nextjs/pages/news/index.js
+++ react_nextjs/pages/news/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link';
 // my-domain.com/news
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">NextJS Is A Great Framework</Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
