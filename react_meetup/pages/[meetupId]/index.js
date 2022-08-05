2b95408427aeba5b4ea4a501f71f6388490117b4
sery <yunsel2@gmail.com>
2022-08-05 17:42:15 +0900

    feat: Add detail component connect with button using programmatical navigation

---
+++ react_meetup/pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1553604503-1ea63c3ba69c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1275&q=80"
+      title="A First Meetup"
+      address="Prague, Czechia"
+      description="This is a first meetup!"
+    />
+  );
+}
+export default MeetupDetails;
