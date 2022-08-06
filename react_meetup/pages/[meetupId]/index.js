23c06d909a045468fd19909343faf92ce3d1447f
sery <yunsel2@gmail.com>
2022-08-07 00:16:05 +0900

    feat: How to prepare paths with getStaticPaths & work wiht fallback

--- react_meetup/pages/[meetupId]/index.js
+++ react_meetup/pages/[meetupId]/index.js
@@ -10,4 +10,41 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://images.unsplash.com/photo-1553604503-1ea63c3ba69c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1275&q=80',
+        id: 'm1',
+        title: 'A First Meetup',
+        address: 'Prague, Czechia',
+        description: 'This is a first meetup!',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
