19be725ab1b0b457eade196fab6d67a8ec0e06bc
sery <yunsel2@gmail.com>
2022-08-22 15:44:01 +0900

    feat: Add newtodo component pass function props to the app component

--- typescript/react-ts/src/App.tsx
+++ typescript/react-ts/src/App.tsx
@@ -1,10 +1,15 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
   const todos = [new Todo('🍒'), new Todo('🍤')];
+
+  const addTodoHandler = (todoText: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
